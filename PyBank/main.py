cb2bc4cc5f7262b3b85e1644929a4f92ed753e30
Fox IV <73198491+FAufenger@users.noreply.github.com>
2020-11-24 09:46:54 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,16 +18,16 @@ greatest_dec = {"month": "", "value": 0}
 with open(csvpath) as csvfile: 
     csvreader = csv.reader(csvfile, delimiter=",")
     for row in csvreader:
-        total_months = total_months + row[1]
+        total_months = total_months + row[0]
         months_sum += 1
         average_change = months_sum / total_months
 
         if greatest_inc["value"] > row[1]:
             greatest_inc["month"] = row[0]
-            greatest_inc["value"] = row[2]
-        if greatest_dec["value"] < row[2]:
+            greatest_inc["value"] = row[1]
+        if greatest_dec["value"] < row[1]:
             greatest_dec["month"] = row[0]
-            greatest_dec["value"] = row[2]
+            greatest_dec["value"] = row[1]
 
 
 output = (
