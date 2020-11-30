ab1b7516d9a59c67f424e910c1ef143644400eb8
Fox IV <73198491+FAufenger@users.noreply.github.com>
2020-11-30 18:43:23 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,11 +6,10 @@ import csv
 #set path for the file
 csvpath = os.path.join("resources", "pypoll_election_data.csv")
 total_votes = 0
-candidate_names = str()
+candidate_names = []
 percent_votes = 0
-candidate_votes = 0
-winner = str()
-
+candidate_votes = {}
+winner = str() #or ""
 
 
 with open(csvpath) as csvfile: 
@@ -18,17 +17,18 @@ with open(csvpath) as csvfile:
     for row in csvreader:
 
         #Total number of votes cast
-        total_votes = total_votes + row[0]
+        total_votes += 1
 
-        #List of all canadites
-        candidate_names = #unique values in first column .. maybe .. 
-        #if (value <> vlaue then...cell value = cell value.. )
-
-        #Total number of votes each candadate won
-        candidate_votes = #if unique candidate then sum column 2
+        #List of all unique canadites and total votes for each candidate
+        if row[2] not in candidate_names:
+            candidate_names.append(row[2])
+            candidate_votes[row[2]] = 1 
+            
+        else:
+            candidate_votes[row[2]] += 1
 
         #Percentage of votes each candidate received
-        percent_votes = (candidate_votes / total_votes)"%"
+        percent_votes = (candidate_votes / total_votes)
 
         #Winner of popular vote
         winner = #candidate votes > other candidates then
@@ -39,7 +39,7 @@ output = (
     f"----------------------------------\n",
     f"Total Votes: {total_votes}\n",
     f"----------------------------------\n",
-    f"{candidate_names}: {percent_votes} ({candidate_votes})\n",
+    f"{#candidate_names}: {percent_votes} ({candidate_votes})\n",
     f"----------------------------------\n",
     f"winner: {winner}\n",
     f"----------------------------------\n"
