d07f8a7694b860fe3fb0d957a7a20ab8213f5c4e
Fox IV <73198491+FAufenger@users.noreply.github.com>
2020-12-02 11:46:11 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,23 +18,29 @@ with open(csvpath) as csvfile:
     for candidate_data in csvreader:
 
         #Total number of votes cast
+        #Adds total of all rows (-header due to above code)
         total_votes += 1
 
-        #List of all unique canadites and start dictionary(set to zero)
+        #List of all unique canadites 
+        #start dictionary(set keys to zero)
         if candidate_data[2] not in candidate_names:
             candidate_names.append(candidate_data[2])
             candidate_name_and_votes_dic[candidate_data[2]] = 0
 
-        #Add total of votes per candidate to dictionary keys
+        #Add total of votes to dictionary keys (unique candidate names)
         candidate_name_and_votes_dic[candidate_data[2]] += 1
+      
 
     #Percentage of votes each candidate received
-    percent_vote1 = round((int(candidate_name_and_votes_dic['Khan']) / int(total_votes)*100),2)
-    percent_vote2 = round((int(candidate_name_and_votes_dic['Correy']) / int(total_votes)*100),2)
-    percent_vote3 = round((int(candidate_name_and_votes_dic['Li']) / int(total_votes)*100),2)
-    percent_vote4 = round((int(candidate_name_and_votes_dic["O'Tooley"]) / int(total_votes)*100),2)
+    percent_vote1 = f"{round(((candidate_name_and_votes_dic[candidate_names[0]]) / (total_votes)*100),2)} %" 
+    percent_vote2 = round(((candidate_name_and_votes_dic[candidate_names[1]]) / (total_votes)*100),2)
+    percent_vote3 = round(((candidate_name_and_votes_dic[candidate_names[2]]) / (total_votes)*100),2)
+    percent_vote4 = round(((candidate_name_and_votes_dic[candidate_names[3]]) / (total_votes)*100),2)
 
 
+candidate_name_and_votes_dic[candidate_names[0]] = [percent_vote1, candidate_name_and_votes_dic[candidate_names[0]]]
+
+    
     #Winner of popular vote
     #winner = #candidate votes > other candidates then
 
