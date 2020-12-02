19218ad9cc521ba8aa596f05038a2fc62c67e690
Fox IV <73198491+FAufenger@users.noreply.github.com>
2020-12-02 10:56:03 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,32 +8,31 @@ csvpath = os.path.join("resources", "pypoll_election_data.csv")
 total_votes = 0
 candidate_names = []
 percent_votes = 0
-candidate_votes = {}
+candidate_name_and_votes_dic = {}
 winner = str() #or ""
-
 
 with open(csvpath) as csvfile: 
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
 
-    for row in csvreader:
+    for candidate_data in csvreader:
 
         #Total number of votes cast
         total_votes += 1
 
-        #List of all unique canadites and start dictionary
-        if row[2] not in candidate_names:
-            candidate_names.append(row[2])
-            candidate_votes[row[2]] = 0
+        #List of all unique canadites and start dictionary(set to zero)
+        if candidate_data[2] not in candidate_names:
+            candidate_names.append(candidate_data[2])
+            candidate_name_and_votes_dic[candidate_data[2]] = 0
 
-        #Add total of votes per candidate in dictionary
-        candidate_votes[row[2]] += 1
+        #Add total of votes per candidate to dictionary keys
+        candidate_name_and_votes_dic[candidate_data[2]] += 1
 
     #Percentage of votes each candidate received
-    percent_vote1 = round((int(candidate_votes['Khan']) / int(total_votes)*100),2)
-    percent_vote2 = round((int(candidate_votes['Correy']) / int(total_votes)*100),2)
-    percent_vote3 = round((int(candidate_votes['Li']) / int(total_votes)*100),2)
-    percent_vote4 = round((int(candidate_votes["O'Tooley"]) / int(total_votes)*100),2)
+    percent_vote1 = round((int(candidate_name_and_votes_dic['Khan']) / int(total_votes)*100),2)
+    percent_vote2 = round((int(candidate_name_and_votes_dic['Correy']) / int(total_votes)*100),2)
+    percent_vote3 = round((int(candidate_name_and_votes_dic['Li']) / int(total_votes)*100),2)
+    percent_vote4 = round((int(candidate_name_and_votes_dic["O'Tooley"]) / int(total_votes)*100),2)
 
 
     #Winner of popular vote
@@ -45,7 +44,7 @@ output = (
     f"----------------------------------\n"
     f"Total Votes: {total_votes}\n"
     f"----------------------------------\n"
-    f"{candidate_votes}\n"
+    f"{candidate_name_and_votes_dic}\n"
     f"----------------------------------\n"
     f"{percent_vote1}%\n"
     f"{percent_vote2}%\n"
